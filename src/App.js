1a8a7830bbf88412ad0e8e343474bb679ee507ae
Tushar Asodariya <tushar.asodariya@niveussolutions.com>
2024-06-12 19:23:56 +0530

    first component created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Item from './my_item.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Item></Item>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/my_item.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Item extends React.Component{
+  render(){
+    return (<h1>Hello World <small>This is Tushar</small></h1>)
+  }
+}
+
+
+export default Item
