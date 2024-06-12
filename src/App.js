52bdcb71dc9fe3325d4735a233c681cfa39c3b21
Tushar Asodariya <tushar.asodariya@niveussolutions.com>
2024-06-12 19:42:29 +0530

    component click event added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Item></Item>
+        <Item name="Tushar"/>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/my_item.js
+++ src/my_item.js
@@ -1,10 +1,17 @@
 import React from "react";
 
-class Item extends React.Component{
-  render(){
-    return (<h1>Hello World <small>This is Tushar</small></h1>)
+class Item extends React.Component {
+  render() {
+    return (
+      <h1 onClick={this.onClickHelloWorld}>
+        Hello World <small>This is {this.props.name}</small>
+      </h1>
+    );
+  }
+
+  onClickHelloWorld(){
+    console.log('Hello World is clicked');
   }
 }
 
-
-export default Item
+export default Item;
