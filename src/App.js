18771257ec448c87c5a3dc97e0859135e74163a4
Tushar Asodariya <tushar.asodariya@niveussolutions.com>
2024-06-15 12:29:01 +0530

    component state added, starwar component added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
-import Item from './my_item.js'
+// import Item from './my_item.js'
+class StarWars extends React.Component {
+
+  constructor(){
+  super()
+    this.state = {name:null,height:null, homeworld:null, films:[]}
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <p>{this.state.height} (cm)</p>
+        <p>Homeworld : {this.state.homeworld}</p>
+        <ul>
+          <li>{this.state.films}</li>
+        </ul>
+        <button type="button" onClick={this.getNewCharacter} className="btn">Randomize Character</button>
+      </div>
+
+    );
+  }
+
+  getNewCharacter = ()=> {
+    console.log('new char');
+    this.setState( {name:"luke",height:"171", homeworld:"skdjf", films:["sdfsdf"]})
+  }
+
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Item name="Tushar"/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <StarWars />
       </header>
     </div>
   );
--- src/my_item.js
+++ src/my_item.js
@@ -1,17 +1,35 @@
 import React from "react";
 
 class Item extends React.Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+    clicks:0
+    }
+  }
+
   render() {
     return (
+    <div>
       <h1 onClick={this.onClickHelloWorld}>
         Hello World <small>This is {this.props.name}</small>
       </h1>
+      <span>
+      {this.state.clicks} is the number of clicks
+      </span>
+      </div>
     );
   }
 
   onClickHelloWorld(){
     console.log('Hello World is clicked');
+    this.setState({
+      clicks:this.state.clicks+1
+    })
   }
 }
+
 
 export default Item;
