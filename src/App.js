2c9af7ff47d4fac9bcfd75663af8d4117e115c5d
Tushar Asodariya <tushar.asodariya@niveussolutions.com>
2024-06-18 17:52:21 +0530

    api call added

--- src/App.js
+++ src/App.js
@@ -3,30 +3,76 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 // import Item from './my_item.js'
+import FilmItemRow from './film_item_row';
 class StarWars extends React.Component {
 
-  constructor(){
-  super()
-    this.state = {name:null,height:null, homeworld:null, films:[]}
+  constructor() {
+    super()
+    this.state = { name: null, height: null, homeworld: null, films: [], loadedCharacter: false }
   }
   render() {
+
+    const movies = this.state.films.map((filmName, i) => {
+
+      return <FilmItemRow key={i} name={filmName} />
+
+
+    })
     return (
-      <div>
-        <h1>{this.state.name}</h1>
-        <p>{this.state.height} (cm)</p>
-        <p>Homeworld : {this.state.homeworld}</p>
-        <ul>
-          <li>{this.state.films}</li>
-        </ul>
+      <div >
+        {
+          this.state.loadedCharacter &&
+          <div>
+            <h1>{this.state.name}</h1>
+            <p>{this.state.height} (cm)</p>
+            <p>Homeworld: {this.state.homeworld}</p>
+            <p>Films: </p>
+            <ul>
+              {
+                movies
+              }
+            </ul>
+          </div>
+        }
+
         <button type="button" onClick={this.getNewCharacter} className="btn">Randomize Character</button>
       </div>
 
     );
   }
 
-  getNewCharacter = ()=> {
-    console.log('new char');
-    this.setState( {name:"luke",height:"171", homeworld:"skdjf", films:["sdfsdf"]})
+  getNewCharacter = () => {
+    const randomNumber = Math.round(Math.random() * 82)
+    const url = `https://swapi.dev/api/people/${randomNumber}`
+    fetch(url).then(response => response.json()).then(data => {
+
+      fetch(data.homeworld).then(homeResp => homeResp.json()).then(async homeData => {
+
+
+        const filmNameResponses = await Promise.all(data.films.map(filmUrl => {
+
+          return fetch(filmUrl).then(resp => resp.json())
+
+        }))
+        let filmNames = []
+        filmNameResponses.map(filmName => {
+          filmNames.push(filmName.title)
+
+        }
+
+        )
+        this.setState({
+          name: data.name, height: data.height, homeworld: homeData.name,
+          films: filmNames,
+          loadedCharacter: true
+        })
+
+
+
+      })
+
+
+    })
   }
 
 }
---
+++ src/film_item_row.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class FilmItemRow extends React.Component{
+render(){
+return (
+
+    <li>
+   
+      {this.props.name}
+    
+    </li>
+);
+
+}
+
+}
+
+
+
+export default FilmItemRow
